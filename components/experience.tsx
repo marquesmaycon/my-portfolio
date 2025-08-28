3b6dd11b6c490f873c88a329b7d53fbce391316d
Maycon Marques <mayconmarquesh@gmail.com>
2025-08-28 12:27:36 -0300

    work experience section, moving border ui

---
+++ components/experience.tsx
@@ -0,0 +1,30 @@
+import { workExperience } from "@/data"
+import { Button } from "./ui/moving-border"
+
+export const Experience = () => {
+  return (
+    <div className="py-20" id="experience">
+      <h1 className="heading">
+        Minha <span className="text-purple-500">Experiência Profissional</span>
+      </h1>
+      <div className="w-full mt-12 grid grid-cols-1 gap-10 lg:grid-cols-4">
+        {workExperience.map((card) => (
+          <Button
+            key={card.id}
+            borderRadius="1.75rem"
+            duration={Math.floor(Math.random() * 10000) + 10000}
+            className="flex-1 text-white border-neutral-200 dark:border-slate-800"
+          >
+            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+              <img src={card.thumbnail} alt={card.title} className="w-16 md:w-20 lg:w-32" />
+            </div>
+            <div className="lg:ms-5">
+              <h2 className="text-start text-xl md:text-2xl font-bold">{card.title}</h2>
+              <p className="text-start text-neutral-200 mt-3 font-semibold">{card.desc}</p>
+            </div>
+          </Button>
+        ))}
+      </div>
+    </div>
+  )
+}
--- data/index.ts
+++ data/index.ts
@@ -2,6 +2,7 @@ export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
   { name: "Testimonials", link: "#testimonials" },
+  { name: "Work Experience", link: "#experience" },
   { name: "Contact", link: "#contact" },
 ]
 
