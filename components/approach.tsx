0cfb966898b524f1eac8e6af0aa458c0ec445e40
Maycon Marques <mayconmarquesh@gmail.com>
2025-08-28 16:42:44 -0300

    aproach card section

---
+++ components/approach.tsx
@@ -0,0 +1,116 @@
+"use client"
+
+import { AnimatePresence, motion } from "motion/react"
+import { useState } from "react"
+
+import { CanvasRevealEffect } from "./ui/canvas-reveal-effect"
+
+export const Approach = () => {
+  return (
+    <section className="w-full py-20">
+      <h2 className="heading">
+        Minha <span className="text-purple-300">Abordagem</span>
+      </h2>
+      <div className="my-20 flex flex-col lg:flex-row items-center justify-center gap-4">
+        <Card
+          title="Planajemaneto & Estratégia"
+          description="Vamos colaborar para listar os objetivos, público alvo e funcionalidades chave do projeto"
+          icon={<AceternityIcon order="Phase 1" />}
+        >
+          <CanvasRevealEffect animationSpeed={5.1} containerClassName="bg-emerald-900" />
+        </Card>
+        <Card title="Nisha is Munni" description="Vamos explorar a relação entre Nisha e Munni" icon={<AceternityIcon order="Phase 2" />}>
+          <CanvasRevealEffect
+            animationSpeed={3}
+            containerClassName="bg-black"
+            colors={[
+              [236, 72, 153],
+              [232, 121, 249],
+            ]}
+            dotSize={2}
+          />
+        </Card>
+        <Card title="Munni is Aditi" description="Vamos explorar a relação entre Munni e Aditi" icon={<AceternityIcon order="Phase 3" />}>
+          <CanvasRevealEffect animationSpeed={3} containerClassName="bg-sky-600" colors={[[125, 211, 252]]} />
+        </Card>
+      </div>
+    </section>
+  )
+}
+
+const Card = ({
+  title,
+  description,
+  icon,
+  children,
+}: {
+  title: string
+  description: string
+  icon: React.ReactNode
+  children?: React.ReactNode
+}) => {
+  const [hovered, setHovered] = useState(false)
+  return (
+    <figure
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem] lg:h-[35rem] rounded-3xl"
+    >
+      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+
+      <AnimatePresence>
+        {hovered && (
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="h-full w-full absolute inset-0">
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <div className="relative z-20">
+        <div className="text-center group-hover/canvas-card:-translate-y-4 group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
+          {icon}
+        </div>
+        <h2 className="dark:text-white text-3xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+          {title}
+        </h2>
+        <h2 className="dark:text-white text-sm opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-purple-300 group-hover/canvas-card:-translate-y-2 transition duration-200 text-center ">
+          {description}
+        </h2>
+      </div>
+    </figure>
+  )
+}
+
+const AceternityIcon = ({ order }: { order: string }) => {
+  return (
+    <button
+      type="button"
+      className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+    >
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-2 text-xl font-bold text-white backdrop-blur-3xl">
+        {order}
+      </span>
+    </button>
+  )
+}
+
+export const Icon = ({ className, ...rest }: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className={className}
+      {...rest}
+    >
+      <title>Icon</title>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+    </svg>
+  )
+}
